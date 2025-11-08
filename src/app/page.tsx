041ed9171036a8e0ad1a73f5596e1286940db6a8
Sree Ram <srir64069@gmail.com>
2025-11-08 09:28:05 +0000

    i think while running the application, it is taking more to load the pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
+import { redirect } from 'next/navigation';
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function SetupPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/login");
-  }, [router]);
-
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-lg">Redirecting to login...</div>
-    </div>
-  );
+export default function RootPage() {
+  redirect('/login');
 }
